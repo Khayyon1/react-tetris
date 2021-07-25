5d187ee5382520dd841dfd6eb7842599064fe0cf
Khayyon1 <k_parker@coloradocollege.edu>
2021-07-24 22:35:25 -0400

    Added initial score board

--- react-tetris/src/App.js
+++ react-tetris/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import GridBoard from './components/GridBoard';
 import nextBlock from './components/nextBlock';
+import ScoreBoard from './components/ScoreBoard';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       </header>
       <GridBoard />
       <nextBlock />
+      <ScoreBoard/>
     </div>
   );
 }
